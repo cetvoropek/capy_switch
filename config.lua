61803f1a10468d2544de0f29014ff9638e2125f8
Dev Capy <78591839+mr-capy@users.noreply.github.com>
2022-11-15 16:47:48 +0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+Config = {
+   
+    inventory = 'ox_inventory',              -- options can be default, ox_inventory
+    
+    useprogressbar = false,             -- if u wanna use progressbar write function in client side
+    
+    switchItem = 'fullautoswitch',       --item name for switch
+    
+    unswitchcommand = 'r_switch',       --unswitch command name here
+    
+--animation
+
+    animDict = 'missmic4',
+    animName = 'michael_tux_fidget',
+    animduration = 5                   --in seconds
+    
+}
